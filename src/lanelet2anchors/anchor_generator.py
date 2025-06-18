23fc702bfd86b1ce11400e1953b71234a46712dc
Thanh <truongthanh@u.nus.edu>
2025-06-18 21:07:31 +0700

    Update Jupyter Notebook

--- src/lanelet2anchors/anchor_generator.py
+++ src/lanelet2anchors/anchor_generator.py
@@ -34,7 +34,6 @@ class AnchorGenerator:
         origin_latitude: float,
         origin_longitude: float,
     ) -> None:
-        print('init AnchorGenerator')
         origin = Origin(origin_latitude, origin_longitude)
         projector = UtmProjector(origin)
         traffic_rules = lanelet2.traffic_rules.create(
