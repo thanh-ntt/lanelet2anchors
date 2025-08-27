24135edf6943ee5ff52f825c5c13055d6dbad9bb
Thanh <truongthanh@u.nus.edu>
2025-08-27 14:56:22 +0800

    Add debug log

--- src/lanelet2anchors/anchor_generator.py
+++ src/lanelet2anchors/anchor_generator.py
@@ -316,8 +316,7 @@ class AnchorGenerator:
                 id2lanelet[ll_id] = ll_match.lanelet
             lanelet_ids.append(cur_pose_ll_ids)
 
-        debug = '18790' in lanelet_ids and '16549' in lanelet_ids
-        if debug:
+        if '18790' in lanelet_ids[0] and '16549' in lanelet_ids[0]:
             for ll_id, lanelet in id2lanelet.items():
                 print(f'{ll_id}: {lanelet}')
         for u_id, u in id2lanelet.items():
